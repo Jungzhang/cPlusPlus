bab124f7090c586613829cfb7c81c90ba6641200
Jungzhang <zhanggen.jung@gmail.com>
2015-11-18 14:23:44 +0800

    add

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 3.cpp
+	> Author: jungzhang
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年11月18日 星期三 12时57分09秒
+	> Description: cout << "Hello World" << endl;的等效版本
+ *************************************************************************/
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     operator<< (cout,"Hello World\n");
+     
+     return 0;
+ }
