79de5f692b47eb302bbc7aaf9f34969b4b83d90f
Jungzhang <zhanggen.jung@gmail.com>
2015-12-05 23:33:19 +0800

    add

---
+++ 3_44.cpp
@@ -0,0 +1,51 @@
+/*************************************************************************
+	> File Name: 3_44.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年12月05日 星期六 22时28分18秒
+	> Description: 练习3.44
+ ************************************************************************/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int ia[3][4], i = 100;
+
+    using int_array = int[4];
+    using int_ptr = int[4];
+
+    for (int_array &a : ia){
+        for (int &b : a){
+            b = i;
+            i++;
+        }
+    }
+    
+    //版本1,范围for语句,注意使用引用
+    for (int_array &a : ia){
+        for (auto b : a){
+            std::cout << b << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    //版本2,普通for语句,但使用下标运算符
+    for (size_t i = 0; i != 3; i++){
+        for (size_t j = 0; j != 4; j++){
+            std::cout << ia[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    //版本3,普通for语句,但使用指针
+    for (int_ptr *i = ia; i != ia + 3; i++){   //i是int a[4]型数组的指针
+        for (int *j = *i; j != *i+4; j++){      //j是int型的指针,对i取值后就是int a[4]型数组
+            std::cout << *j << "\t";
+        }
+        std::cout << std::endl;
+    }
+    
+    return 0;
+}
