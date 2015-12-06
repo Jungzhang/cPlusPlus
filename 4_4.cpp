78835b06b6b3396dc85628c73b1f32b137e8f2ce
Jungzhang <zhanggen.jung@gmail.com>
2015-12-06 16:20:03 +0800

    add

---
+++ 4_4.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 4_4.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年12月06日 星期日 16时18分25秒
+	> Description: 习题4.4
+ ************************************************************************/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << std::endl;
+    
+    return 0;
+}
