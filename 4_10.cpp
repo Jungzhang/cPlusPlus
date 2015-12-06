b35380c8f7c62ff763becf9f96363e2a3f221176
Jungzhang <zhanggen.jung@gmail.com>
2015-12-06 16:58:41 +0800

    add

---
+++ 4_10.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 4_10.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年12月06日 星期日 16时54分32秒
+	> Description: 习题4.10
+ ************************************************************************/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int num = 0;
+
+    while(num != 42){
+        std::cin >> num;
+    }
+    
+    return 0;
+}
