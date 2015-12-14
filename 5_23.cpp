c5e68b71e0f9b9326bb39fc90719a905c918dfb7
Jungzhang <zhanggen.jung@gmail.com>
2015-12-14 19:52:41 +0800

    add

---
+++ 5_23.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 5_23.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2015年12月14日 星期一 19时46分14秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+
+    std::cin >> num1 >> num2;
+
+    std::cout << num1 / num2 << std::endl;
+    
+    return 0;
+}
