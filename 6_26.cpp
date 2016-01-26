57ea9361f29ec4357f114d1df1d0c3c677d48ee7
Jungzhang <zhanggen.jung@gmail.com>
2016-01-26 12:59:40 +0800

    add

---
+++ 6_26.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 6_26.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年01月26日 星期二 12时56分03秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    for (int a = 1; a < argc; ++a) {
+        std::cout << argv[a] << std::endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
