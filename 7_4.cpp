877d848a887a98f2db91618192bd8589797a2820
Jungzhang <zhanggen.jung@gmail.com>
2016-02-02 14:43:43 +0800

    add

---
+++ 7_4.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 7_4.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月02日 星期二 14时39分27秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+struct Person {
+    std::string name;
+    std::string adder;
+};
+
+int main(int argc, char *argv[])
+{
+    
+    return EXIT_SUCCESS;
+}
