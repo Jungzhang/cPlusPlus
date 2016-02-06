5d9edb9bfacce241f9f0996ae08dcae8b018efe6
Jungzhang <zhanggen.jung@gmail.com>
2016-02-06 16:15:04 +0800

    ad

---
+++ 8_4.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+	> File Name: 8_4.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月06日 星期六 15时52分02秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+
+void func(std::string fileName, std::vector<std::string> &s)
+{
+    if (fileName.empty()) {
+        std::cout << "文件名不正确" << std::endl;
+        return;
+    }
+    std::ifstream file(fileName);
+    if (file) {
+        std::string temp;
+        while(getline(file, temp)) {
+            s.push_back(temp);
+        }
+    }
+    file.close();
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> s;
+    func("text.txt", s);
+    for(auto a : s) {
+        std::cout << a << std::endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
