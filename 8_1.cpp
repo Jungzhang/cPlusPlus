6077eed4ed1d6dff1314e505947f3a0a9aae0fe3
Jungzhang <zhanggen.jung@gmail.com>
2016-02-06 15:10:38 +0800

    add

---
+++ 8_1.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 8_1.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月06日 星期六 14时43分31秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+std::istream &func(std::istream &in)
+{
+    std::string s;
+    while(in >> s) {
+        std::cout << s << std::endl;
+    }
+    in.clear();
+
+    return in;
+}
