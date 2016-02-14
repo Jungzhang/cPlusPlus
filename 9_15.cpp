39d1e341a880bd060f59d380babab572eba5df69
Jungzhang <zhanggen.jung@gmail.com>
2016-02-14 15:22:13 +0800

    add

---
+++ 9_15.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: 9_15.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月14日 星期日 15时14分08秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <list>
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> a = {2,2,2,2};
+    std::vector<int> b = {2,2,2,2};
+
+    if (a == b) {
+        std::cout << "相等" << std::endl; 
+    }
+    
+    return EXIT_SUCCESS;
+}
