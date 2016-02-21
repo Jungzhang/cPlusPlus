d614d5b43cfeb0dec873b63c74149477550248be
Jungzhang <zhanggen.jung@gmail.com>
2016-02-21 17:27:02 +0800

    add

---
+++ 10_6.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: 10_6.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月21日 星期日 17时23分03秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    fill_n(v.begin(), v.size(), 0);
+    for (auto a : v) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+    
+    return EXIT_SUCCESS;
+}
