7a971b756d235ae3d6612e4cf220e864aae65806
Jungzhang <zhanggen.jung@gmail.com>
2016-02-22 23:06:18 +0800

    add

---
+++ 10_30.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 10_30.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月22日 星期一 23时00分44秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+int main(int argc, char *argv[])
+{
+    std::istream_iterator<int> in(std::cin), eof;
+    std::ostream_iterator<int> out(std::cout, " ");
+    std::vector<int> vec(in, eof);
+    
+    sort(vec.begin(), vec.end());
+    copy(vec.begin(), vec.end(), out);
+
+    std::cout << std::endl;
+    
+    return EXIT_SUCCESS;
+}
