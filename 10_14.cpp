8c47981807f2986c839af55fc3f1e1b458fc5aa3
Jungzhang <zhanggen.jung@gmail.com>
2016-02-22 12:28:57 +0800

    add

---
+++ 10_14.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 10_14.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月22日 星期一 12时20分09秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    auto sum = [](int a, int b) {return a + b;};
+
+    return EXIT_SUCCESS;
+}
