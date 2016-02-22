abd3529a4f8571e8bd8bd91b2964aaa3d52f0a76
Jungzhang <zhanggen.jung@gmail.com>
2016-02-22 12:32:45 +0800

    aDD

---
+++ 10_15.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 10_15.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月22日 星期一 12时29分19秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    int a = 10;
+    auto b = [a](int c) {return a + c;};
+    
+    std::cout << b(3) << std::endl;
+
+    return EXIT_SUCCESS;
+}
