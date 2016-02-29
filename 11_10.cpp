5d2b2d203b896785965fad0be54b0b9e50377f89
Jungzhang <zhanggen.jung@gmail.com>
2016-02-29 19:24:37 +0800

    add

---
+++ 11_10.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: 11_10.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月25日 星期四 10时51分34秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <list>
+
+int main(int argc, char *argv[])
+{
+    std::map<std::vector<int>::iterator, int> m;
+    std::list<std::list<int>::iterator, int> l;
+    
+    return EXIT_SUCCESS;
+}
