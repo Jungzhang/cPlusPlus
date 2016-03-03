7c994af7a2fce236b0b27c4b6c7b3f5c673dd5df
Jungzhang <zhanggen.jung@gmail.com>
2016-03-03 23:05:05 +0800

    add

---
+++ 12_2.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 12_2.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年03月03日 星期四 22时15分35秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+#include "12_2.h"
+
+int main(int argc, char *argv[])
+{
+    
+    return EXIT_SUCCESS;
+}
