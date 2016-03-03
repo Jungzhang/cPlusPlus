da14ef39f17857039ca074c299356292863646c3
Jungzhang <zhanggen.jung@gmail.com>
2016-03-03 18:31:34 +0800

    add

---
+++ 11_38_1.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: 11_38_1.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年02月23日 星期二 16时34分29秒
+	> Description:
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <map>
+#include <unordered_map>
+
+int main(int argc, char *argv[])
+{
+    std::unordered_map<std::string, size_t> words_count;
+    std::string word;
+
+    while(std::cin >> word) {
+        ++words_count[word];
+    }
+
+    for (auto a : words_count) {
+        std::cout << a.first << "：" << a.second << std::endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
