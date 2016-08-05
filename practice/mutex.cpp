e1df204eb628a64888e1835844a90423bafd6406
zhanggen <zhanggen.jung@gmail.com>
2016-08-05 09:41:11 +0800

    add

--- practice/mutex.cpp
+++ practice/mutex.cpp
@@ -10,6 +10,7 @@
 //
 
 std::mutex g_lock; //独占性互斥量
+//std::recursive_mutex mutex; //递归锁,允许一个线程多次获得同一把锁
 
 //普通使用
 void testMutex()
@@ -27,18 +28,57 @@ void testMutex1()
     sleep(1);
 }
 
+//递归锁
+struct Complex {
+//    std::recursive_mutex mutex;    //递归锁
+    std::mutex mutex; //普通锁，当使用这个的时候会导致程序死锁
+
+    int i;
+
+    Complex(int i) : i(i){}
+
+    void mul(int x)
+    {
+//        std::lock_guard<std::recursive_mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(mutex);
+        i *= x;
+        std::cout << i << std::endl;
+    }
+
+    void div(int x)
+    {
+//        std::lock_guard<std::recursive_mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(mutex);
+        i /= x;
+        std::cout << i << std::endl;
+    }
+
+    void both(int x, int y)
+    {
+//        std::lock_guard<std::recursive_mutex> lock(mutex);
+        std::lock_guard<std::mutex> lock(mutex);
+        std::cout << "哈哈啊哈" << std::endl;
+        mul(x);
+        div(y);
+    }
+};
+
 int main(int argc, char * argv[])
 {
 
-    std::thread t1(testMutex1);
-    std::thread t2(testMutex1);
-    std::thread t3(testMutex1);
-    std::thread t4(testMutex1);
+//    std::thread t1(testMutex1);
+//    std::thread t2(testMutex1);
+//    std::thread t3(testMutex1);
+//    std::thread t4(testMutex1);
+//
+//    t1.join();
+//    t2.join();
+//    t3.join();
+//    t4.join();
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
+    Complex complex(5);
+    complex.both(10,20);
+
 
     return EXIT_SUCCESS;
 }
