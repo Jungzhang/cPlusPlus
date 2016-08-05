96a6b30af5748000e9d5c8301e9e6e6ef8957d67
zhanggen <zhanggen.jung@gmail.com>
2016-08-05 08:51:53 +0800

    add

--- practice/thread.cpp
+++ practice/thread.cpp
@@ -1,7 +1,24 @@
 //
 // Created by zhanggen on 16-8-5.
 //
+
+#include <thread>
+#include <iostream>
+
+void func1()
+{
+
+}
+
+//线程的基本用法
 int main(int argc, char *argv[])
 {
+    std::thread t(func1);
+    std::cout << t.get_id() << std::endl;
+
+    std::cout << std::thread::hardware_concurrency() << std::endl;
+
+    t.join();
+
     return 0;
 }
