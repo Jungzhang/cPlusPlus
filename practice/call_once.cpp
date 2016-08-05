42c7e5dcfe938d0373ea63582ff5eb94a98f4227
zhanggen <zhanggen.jung@gmail.com>
2016-08-05 17:47:12 +0800

    add

---
+++ practice/call_once.cpp
@@ -0,0 +1,27 @@
+//
+// Created by zhanggen on 16-8-5.
+//
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+std::once_flag flag;
+
+void do_once()
+{
+    std::call_once(flag, [](){ std::cout << "Caled once" << std::endl;});
+}
+
+int main(int argc, char *argv[])
+{
+    std::thread t1(do_once);
+    std::thread t2(do_once);
+    std::thread t3(do_once);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
